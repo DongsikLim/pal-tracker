e0b9266eb680bacac732af72ea4342e19d04fe4b
Dongsik Lim <csco3pl@gmail.com>
2017-09-06 14:56:53 +0800

    anotaion

--- src/main/java/io/pivotal/pal/tracker/PalTrackerApplication.java
+++ src/main/java/io/pivotal/pal/tracker/PalTrackerApplication.java
@@ -3,7 +3,12 @@ package io.pivotal.pal.tracker;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 
+@ComponentScan({
+        "io.pivotal.pal.tracker",
+        "io.pivotal.pal.welcome"
+})
 @SpringBootApplication
 public class PalTrackerApplication {
 
