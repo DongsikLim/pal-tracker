4d0c2265225edc9a9d299f0fc365dc6c6436c335
Dongsik Lim <csco3pl@gmail.com>
2017-09-06 09:32:02 +0800

    api message

--- src/main/java/io/pivotal/pal/tracker/WelcomeController.java
+++ src/main/java/io/pivotal/pal/tracker/WelcomeController.java
@@ -15,7 +15,7 @@ public class WelcomeController {
 
     @GetMapping("/")
     public String sayHello() {
-        return "Hello from Cloud Foundry";
+        return "A welcome message";
     }
 
 }
