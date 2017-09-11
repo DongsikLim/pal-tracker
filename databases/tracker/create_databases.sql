45b2c9b86976ca743e3af1e840505a67d25174ed
Dongsik Lim <csco3pl@gmail.com>
2017-09-11 09:44:11 +0800

    tracker account changed

--- databases/tracker/create_databases.sql
+++ databases/tracker/create_databases.sql
@@ -3,7 +3,7 @@ DROP DATABASE IF EXISTS tracker_test;
 
 CREATE USER 'tracker@localhost'
   IDENTIFIED BY '';
-GRANT ALL PRIVILEGES ON *.* TO 'tracker' @'localhost';
+GRANT ALL PRIVILEGES ON *.* TO 'tracker@localhost';
 
 CREATE DATABASE tracker_dev;
 CREATE DATABASE tracker_test;
